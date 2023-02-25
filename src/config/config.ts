c40356f63125f2825c78c6621f2ac17bb46bd922
ihebab99 <ihebabdelmoumen3@gmail.com>
2023-02-25 14:43:50 +0100

    config

---
+++ src/config/config.ts
@@ -0,0 +1,10 @@
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+
+export const MysqlConfig: MysqlConnectionOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: '',
+  database: 'nest-app',
+};
