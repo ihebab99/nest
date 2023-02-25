acf1f2a50c6030845115d4056b7adf48a8ec382f
ihebab99 <ihebabdelmoumen3@gmail.com>
2023-02-25 14:41:29 +0100

    commit to tp

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MysqlConfig } from './config/config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(MysqlConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
